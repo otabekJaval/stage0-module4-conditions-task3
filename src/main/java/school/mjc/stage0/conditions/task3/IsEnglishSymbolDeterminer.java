545ed5025636d5d6948e609adb54d4222393504a
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-02 20:07:51 +0500

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.Calendar;
+
 public class IsEnglishSymbolDeterminer {
+    public static void main(String[] args) {
+
+    }
     public void isEnglishSymbol(char symbol) {
+        if (Character.isAlphabetic(symbol)&&symbol!='Ł'&&symbol!='ł') {
+            System.out.println("English");
+        }else{
+            System.out.println("Non English");
+        }
     }
 }
