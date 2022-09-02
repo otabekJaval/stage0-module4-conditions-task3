0012f5e8ab141a9e5f8cc1d94dd2a9a075789dba
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-02 20:07:00 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.task3;
 
+import javax.security.auth.callback.CallbackHandler;
+
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        character = Character.toLowerCase(character);
+        if (character == 'a' || character == 'e' || character == 'u' ||
+                character == 'o' || character == 'i') {
+            System.out.println("Vowel");
+        } else if (character >= 65 && character <= 122) {
+            System.out.println("Consonant");
+        }else{
+            System.out.println("wrong alphabet!");
+        }
     }
 }
