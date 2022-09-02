761b5b51aaf990c69c940848b2420c59cd3da6f8
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-02 20:07:32 +0500

    Update Divider.java

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task3;
 
 public class Divider {
     public void isDividableBy5And11(int number) {
+        System.out.println(number == 0 ? "cannot divide by zero" : number % 5 == 0 || number % 11 == 0 ? "Dividable" : "Non-dividable");
     }
 }
